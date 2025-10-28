1f16a67cce8068f2b6a5dcefc1a42856132e4ac0
lapitanN17 <lapitannathaniel17@gmail.com>
2025-10-28 15:32:04 +0800

    Refactor StudentInformationSystem class structure

--- StudentInformationSystem.java
+++ StudentInformationSystem.java
@@ -1,9 +1,9 @@
 import java.io.*;
 
-public class Main {
+public class StudentInformationSystem {
     public static void main(String[] args) {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
+                                   //0,       1,        2,        3
         String[] studentNumber = { "69420", "42069", "12345", "14334" };
         String[] studentName = { "Klent Alayon", "Stanley Comidoy", "Micah Ella Dela Peña", "John Evan Abaya" };
         String[] studentGrades = { "75,80,85,90,95", "80,81,82,83,84", "95,75,85,80,90", "95,96,97,98,99" };
@@ -16,51 +16,52 @@ public class Main {
         
         boolean logout = false;
         while (!logout) {
-            for (int i = 0; i < studentNumber.length; i++) {
-                accounts[i] = studentNumber[i] + "," + studentName[i] + "," + studentBalance[i];
-            }
-            int existingAccounts = accounts.length;
-            
-            System.out.print("\n*-----STUDENT PORTAL-----*\n" +
-            "Enter 'LOGIN' to continue\n" + "(Press Enter to Exit)\n" + "-------------------------------------\n" + "Input: "
-            );
-
-            String selectedChoice = null;
             try {
-                selectedChoice = br.readLine();
+                for (int i = 0; i < studentNumber.length; i++) {
+                    //"69420,Klent Alayon,11490"
+                    accounts[i] = studentNumber[i] + "," + studentName[i] + "," + studentBalance[i];
+                }
+                int existingAccounts = accounts.length; //0123
+                
+            
+                System.out.print("\n*-----STUDENT PORTAL-----*\n" +
+                "Enter 'LOGIN' to continue\n" + "(Press Enter to Exit)\n" + "-------------------------------------\n" + "Input: "
+                );
+
+                String selectedChoice = br.readLine();
+                 
+                switch (selectedChoice.toUpperCase().trim()) {
+                   case "LOGIN":
+                        String loggedIn = login(br, accounts, existingAccounts, studentNumber, studentName); //call function
+                        if (loggedIn != null) {
+                            mainInterface(br, accounts, studentNumber, studentName, studentGrades, studentBalance, subjects, timeSchedule, daySchedule, existingAccounts, loggedIn);
+                        }
+                      break;
+                    case "":
+                      break;
+                    default:
+                        System.out.println("Invalid Input! Try Again");
+                }
+            
+            
+                if (selectedChoice.trim().isEmpty()) {
+                    while (true) {
+                        System.out.print("\n-----ARE YOU SURE YOU WANT TO EXIT?-----\nEnter 'YES' to exit\n(Press Enter to return)\nInput: ");
+                    
+                        String selectedChoice2 = br.readLine();
+                        if (selectedChoice2 == null || selectedChoice2.trim().isEmpty()) break;
+                       
+                        if (selectedChoice2.toUpperCase().trim().equals("YES")) {
+                            logout = true;
+                        } else {
+                            System.out.println("Invalid Input! Try again");
+                            continue;
+                        }
+                        break;
+                    } 
+                }
             } catch (IOException e) {
                 System.out.println("Error!");
-                continue;
-            }
-            
-            switch (selectedChoice.toUpperCase().trim()) {
-                case "LOGIN":
-                    String loggedIn = login(br, accounts, existingAccounts, studentNumber, studentName);
-                    if (loggedIn != null) {
-                        mainInterface(br, accounts, studentNumber, studentName, studentGrades, studentBalance, subjects, timeSchedule, daySchedule, existingAccounts, loggedIn);
-                    }
-            }
-            
-            
-            if (selectedChoice == null || selectedChoice.trim().isEmpty()) {
-                while (true) {
-                    System.out.print("\n-----ARE YOU SURE YOU WANT TO EXIT?-----\nEnter 'YES' to exit\n(Press Enter to return)\nInput: ");
-                    
-                    String selectedChoice2 = null;
-                    try {
-                        selectedChoice2 = br.readLine();
-                        if (selectedChoice2 == null || selectedChoice2.trim().isEmpty()) break;
-                    } catch (IOException e) {
-                        System.out.println("Error!");
-                        continue;
-                    }
-                
-                    switch (selectedChoice2.toUpperCase().trim()) {
-                        case "YES":
-                            logout = true;
-                    }
-                    break;
-                }
             }
         }
     }
@@ -92,7 +93,8 @@ public class Main {
             System.out.println("\nInvalid Student Number! Try Again\n");
         }
     }
-
+    
+    
     //Main Interface
     public static void mainInterface(BufferedReader br, String[] accounts, String[] studentNumber, String[] studentName, String[] studentGrades, String[] studentBalance, String[] subjects, String[] timeSchedule, String[] daySchedule, int existingAccounts, String loggedIn) {
         while (true) {
@@ -101,7 +103,10 @@ public class Main {
             
             try {
                 enterInput = br.readLine();
-                if (enterInput == null || enterInput.trim().isEmpty()) return;
+                if (enterInput.trim().isEmpty()) {
+                    System.out.println("\nReturning to Login Page...");
+                    return;
+                }
             } catch (IOException e) {
                 System.out.println("Error");
                 continue;
@@ -109,73 +114,92 @@ public class Main {
             
             switch (enterInput.toUpperCase().trim()) {
                 case "GRADES":
-                    System.out.print("\n-----GRADES-----\n");
-                    
-                    for (int i = 0; i < existingAccounts; i++) {
-                        String[] accountPart = accounts[i].split(",");
-                        String checkStudentNumber = accountPart[0];
+                    while (true) {
+                        System.out.print("\n-----GRADES-----\n");
+                         //0,1 2 3
+                        for (int i = 0; i < existingAccounts; i++) {
+                            // accounts = "69420"    "Klent Alayon"   "balance"   
+                            String[] accountPart = accounts[i].split(",");
+                            String checkStudentNumber = accountPart[0];
                         
-                        if (checkStudentNumber.equals(loggedIn)) {
-                            for (int j = 0; j < subjects.length; j++) {
-                                String[] gradePart = studentGrades[i].split(",");
-                                System.out.println(subjects[j] + "\nAverage: " + gradePart[j] + "\n");
+                            if (checkStudentNumber.equals(loggedIn)) {
+                                for (int j = 0; j < subjects.length; j++) {
+                                    //75, 80, 85, 90, 95
+                                    String[] gradePart = studentGrades[i].split(",");
+                                    System.out.println(subjects[j] + "\nAverage: " + gradePart[j] + "\n");
+                                }
+                                break;
                             }
-                            break;
                         }
-                    }
                     
-                    String input1 = null;
-                    try {
-                        input1 = br.readLine();
-                        if (input1 == null || input1.trim().isEmpty()) break;
-                    } catch (IOException e) {
-                        System.out.println("Error");
-                        continue;
+                        String input1 = null;
+                         try {
+                            input1 = br.readLine();
+                            if (input1.trim().isEmpty()) {
+                                break;
+                            } else {
+                                System.out.println("Invalid Input! Try Again");
+                                continue;
+                            }
+                        } catch (IOException e) {
+                            System.out.println("Error");
+                            continue;
+                       }
                     }
                   break;
                   
                 case "BALANCE":
-                    System.out.print("\n-----BALANCE-----\n");
-                    for (int i = 0; i < existingAccounts; i++) {
-                        String[] accountPart = accounts[i].split(",");
-                        String checkStudentNumber = accountPart[0];
-                        String checkStudentBalance = accountPart[2];
+                    while (true) {
+                        System.out.print("\n-----BALANCE-----\n");
+                        for (int i = 0; i < existingAccounts; i++) {
+                            String[] accountPart = accounts[i].split(",");
+                            String checkStudentNumber = accountPart[0];
+                            String checkStudentBalance = accountPart[2];
                         
-                        if (checkStudentNumber.equals(loggedIn)) {
-                            System.out.println("Your Remaining Balance is: " + checkStudentBalance + "\n");
+                            if (checkStudentNumber.equals(loggedIn)) {
+                                System.out.println("Your Remaining Balance is: " + checkStudentBalance + "\n");
+                            }
                         }
-                    }
                     
-                    String input2 = null;
-                    try {
-                        input2 = br.readLine();
-                        if (input2 == null || input2.trim().isEmpty()) break;
-                    } catch (IOException e) {
-                        System.out.println("Error");
-                        continue;
+                        String input2 = null;
+                        try {
+                            input2 = br.readLine();
+                            if (input2.trim().isEmpty()) {
+                                break;
+                            } else {
+                                System.out.println("Invalid Input! Try Again");
+                                continue;
+                            }
+                        } catch (IOException e) {
+                            System.out.println("Error");
+                            continue;
+                        }
                     }
                   break;
                   
                 case "SCHEDULE":
-                    System.out.print("\n-----SCHEDULE-----\n");
+                    while (true) {
+                        System.out.print("\n-----SCHEDULE-----\n");
                     
-                    for (int i = 0; i < subjects.length; i++) {
-                        System.out.println(subjects[i] + "\nTime: " + timeSchedule[i] + "\nDay: " + daySchedule[i] + "\n");
-                    }
+                        for (int i = 0; i < subjects.length; i++) {
+                            System.out.println(subjects[i] + "\nTime: " + timeSchedule[i] + "\nDay: " + daySchedule[i] + "\n");
+                        }
                     
-                    String input3 = null;
-                    try {
-                        input3 = br.readLine();
-                        if (input3 == null || input3.trim().isEmpty()) break;
-                    } catch (IOException e) {
-                        System.out.println("Error");
-                        continue;
+                        String input3 = null;
+                        try {
+                            input3 = br.readLine();
+                            if (input3.trim().isEmpty()) break;
+                            else System.out.println("Invalid Input! Try Again");
+                        } catch (IOException e) {
+                            System.out.println("Error");
+                            continue;
+                        }
                     }
                   break;
                   
                 case "EVALUATION":
                     while (true ) {
-                        System.out.print("\nHow would you rate [INSERT INTRUCTOR] teaching method?\n");
+                        System.out.print("\nHow would you rate Kyle Dominic Pacatang's teaching method?\n");
                         System.out.print("1 - MEH\n");
                         System.out.print("2 - OK\n");
                         System.out.print("3 - GOOD\n");
@@ -185,7 +209,7 @@ public class Main {
                         String rating = null;
                         try {
                             rating = br.readLine();
-                            if (rating == null || rating.trim().isEmpty()) break;
+                            if (rating.trim().isEmpty()) break;
                         } catch (IOException e) {
                             System.out.println("Error");
                             continue;
@@ -200,10 +224,8 @@ public class Main {
                       break;
                     }
                   break;
-                  
                 default:
-                  System.out.println("Invalid Input! Try Again");
-                  break;
+                    System.out.println("Invalid Input! Try Again");
             }
         }
     }
